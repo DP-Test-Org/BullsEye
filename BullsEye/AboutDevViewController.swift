e396439f283277c6bf974f289c3d3007ab73f5ae
Don 2 <dpcamerino@yahoo.com>
2020-09-25 16:10:15 -0700

    completed lesson 4 about view controllers

---
+++ BullsEye/AboutDevViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutDevViewController.swift
+//  BullsEye
+//
+//  Created by Don Camerino on 9/25/20.
+//
+
+import UIKit
+
+class AboutDevViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closeAboutDev() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
